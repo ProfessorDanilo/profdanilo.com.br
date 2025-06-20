4717cf72687bb1157999b0a007a66485afa0bd4b
danilolimacomp@gmail.com <djkcond@gmail.com>
2025-06-20 00:07:05 -0300

    deu certo automatizar o cron. Agora é só atualizar o caminho

--- cron_loop.sh
+++ cron_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-  /usr/bin/php /home/defaultwebsite/public/moodle/admin/cli/cron.php
+  /usr/local/bin/php /home/defaultwebsite/public/moodle/admin/cli/cron.php
   sleep 60
 done
