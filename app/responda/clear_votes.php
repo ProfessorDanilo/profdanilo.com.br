e73a9afe6f20dff868c73c14020f59e271ec67ea
Danilo Lima <djkcond@gmail.com>
2026-07-26 06:13:11 -0300

    teste4

--- app/responda/clear_votes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = 'votes.txt';
-if (file_exists($file)) {
-    unlink($file);  // Deleta o arquivo de votos
-}
-?>
--- cron_loop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-  /usr/local/bin/php /home/defaultwebsite/public/moodle/admin/cli/cron.php
-  sleep 60
-done
